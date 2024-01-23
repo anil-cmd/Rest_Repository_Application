2f0d7ba141da780db4294a49c1f8be58cc6d9f08
Anil <anilkumargen03@gmail.com>
2024-01-23 05:31:51 -0500

    'commit-msg'

---
+++ src/main/java/anil/it/SpringRestRepositroyApplication.java
@@ -0,0 +1,13 @@
+package anil.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestRepositroyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestRepositroyApplication.class, args);
+	}
+
+}
